0b333f90efccafe109b98f2a5187bc9e0824d30e
Olivier Norture <olivier@norture.fr>
2025-10-12 22:08:23 +0200

    update script

--- overlay/usr/local/bin/change_resolution.sh
+++ overlay/usr/local/bin/change_resolution.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# Save current mode
 xrandr | grep '*' | awk '{print $1}' > /tmp/original_mode
 
-# Get new mode
-WIDTH=$1
-HEIGHT=$2
-MODELINE=$(cvt $WIDTH $HEIGHT 60 | grep Modeline | sed 's/Modeline //')
+MODELINE=$(cvt $SUNSHINE_CLIENT_WIDTH $SUNSHINE_CLIENT_HEIGHT 60 | grep Modeline | sed 's/Modeline //')
 MODENAME=$(echo $MODELINE | awk '{print $1}')
 xrandr --newmode $MODELINE
 xrandr --addmode DP-0 $MODENAME
