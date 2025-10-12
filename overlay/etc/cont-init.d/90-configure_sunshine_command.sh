c8e6b7e21c61f2b438e9ab7c88feef4b2bdc0399
Olivier Norture <olivier@norture.fr>
2025-10-12 22:11:51 +0200

    replace global_prep_cmd

--- overlay/etc/cont-init.d/90-configure_sunshine_command.sh
+++ overlay/etc/cont-init.d/90-configure_sunshine_command.sh
@@ -8,6 +8,9 @@ if [ ! -f "$CONFIG_FILE" ]; then
     touch "$CONFIG_FILE"
 fi
 
-if ! grep -q "global_prep_cmd" "$CONFIG_FILE"; then
-    echo 'global_prep_cmd = [{"do":"/usr/local/bin/change_resolution.sh %SUNSHINE_CLIENT_WIDTH% %SUNSHINE_CLIENT_HEIGHT%","undo":"/usr/local/bin/revert_resolution.sh"}]' >> "$CONFIG_FILE"
+CMD='global_prep_cmd = [{"do":"/usr/local/bin/change_resolution.sh","undo":"/usr/local/bin/revert_resolution.sh"}]'
+if grep -q "global_prep_cmd" "$CONFIG_FILE"; then
+    sed -i "s|^global_prep_cmd.*|$CMD|" "$CONFIG_FILE"
+else
+    echo "$CMD" >> "$CONFIG_FILE"
 fi
